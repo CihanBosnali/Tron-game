8a021dff0776afffc4b19979d06f507990ddbf8e
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-29 16:55:31 +0300

    init func written in lightbike

--- game/classes/lightbike.js
+++ game/classes/lightbike.js
@@ -1,15 +1,18 @@
-export class lightbike(player_id){
-    constructor(){
+export class Lightbike {
+    constructor() {
         this.pos = {};
-        this.pos.x = 10 * player_id;
-        this.pos.y = 10 * player_id;
+        this.pos.x = 10;
+        this.pos.y = 10;
 
         this.light = {};
         this.light.points = [];
         this.light.lenght = 1;
+        this.light.color = "yellow";
     };
 
-    init(){
+    init(player_id) {
+        this.pos.x *= player_id;
+        this.pos.x += 1;
 
     };
 }
