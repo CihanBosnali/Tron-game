b54bb1b37202acd5d27cb48a2f770191e8d25e07
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-29 17:06:31 +0300

    changed id to constructor

--- game/classes/lightbike.js
+++ game/classes/lightbike.js
@@ -1,22 +1,22 @@
 export class Lightbike {
-    constructor() {
-        this.id = 0;
+    constructor(player_id) {
+
+        this.id = player_id;
         this.pos = {};
-        this.pos.x = 10;
+        this.pos.x = 10 * player_id;
         this.pos.y = 10;
 
         this.light = {};
         this.light.points = [];
         this.light.lenght = 1;
         this.light.color = "yellow";
-    }
 
-    init(player_id) {
-        this.id = player_id;
-        this.pos.x *= player_id;
-        this.pos.x += 1;
         if (player_id == 1){
             this.light.color = "green";
+    }
+
+    init() {
+        
         }
     }
 }
