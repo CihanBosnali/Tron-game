d3c7222ecfca877ead170c7f095565a9ba7e8c32
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-29 17:00:15 +0300

    change init by id

--- game/classes/lightbike.js
+++ game/classes/lightbike.js
@@ -1,5 +1,6 @@
 export class Lightbike {
     constructor() {
+        this.id = 0;
         this.pos = {};
         this.pos.x = 10;
         this.pos.y = 10;
@@ -8,11 +9,15 @@ export class Lightbike {
         this.light.points = [];
         this.light.lenght = 1;
         this.light.color = "yellow";
-    };
+    }
 
     init(player_id) {
+        this.id = player_id;
         this.pos.x *= player_id;
         this.pos.x += 1;
+        if (player_id == 1){
+            this.light.color = "green";
+        }
 
-    };
+    }
 }
