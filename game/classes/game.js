7e787ba5b08ac02f93fcc81655f3f908892307eb
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-29 16:49:38 +0300

    lightbike added to game

--- game/classes/game.js
+++ game/classes/game.js
@@ -1,9 +1,12 @@
+import { lightbike } from "./lightbike";
+
 export class Game {
     constructor(){
         this.canvas = document.getElementById("gameCanvas");
         this.context = this.canvas.msGetInputContext("2d");
 
-        this.player1 = 
+        this.player0 = lightbike(0);
+        this.player1 = lightbike(1);
     }
 
     init(){
